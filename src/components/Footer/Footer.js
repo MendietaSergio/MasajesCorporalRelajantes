e2863a9ee762532bfc9f5e9c2854dd87fe510a35
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-08-21 20:15:58 -0300

    Añade mejoras en Footer, agrega copyright y link portafolio

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,23 +4,23 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="container my-4">
+        <div className="container mt-4 mb-2">
           <div className="row">
-            <div className="col-4 ">
-              <h5 className="text-center">Sobre mí</h5>
+            <div className="col-4 vertical-line">
+              <h5 className="text-center footer_title">Sobre mí</h5>
               <p className="footer_about">
-                Brindamos la mejor experiencia posible para ayudarlo a ser
+                Brindo la mejor experiencia adquirida para ayudarlo a ser
                 saludable, productivo y vivir mejor.
               </p>
             </div>
-            <div className="col-4 ">
-              <h5 className="text-center">Horarios de apertura</h5>
+            <div className="col-4 vertical-line">
+              <h5 className="text-center footer_title">Horarios de apertura</h5>
               <p className="footer_openHours">
                 Lunes a Viernes de 09:00hs. a 20hs.
               </p>
             </div>
-            <div className="col-4 ">
-              <h5 className="text-center">Contacto</h5>
+            <div className="col-4 vertical-line">
+              <h5 className="text-center footer_title">Contacto</h5>
               <p className="footer_contact">
                 <i class="fas fa-map-marker-alt"></i> Humberto Primo 6496, San
                 Francisco Solano, Provincia de Buenos Aires
@@ -29,7 +29,24 @@ const Footer = () => {
                 <i class="fas fa-mobile-alt"></i> 1134585757
               </p>
             </div>
-            <hr className="shadow"/>
+            <hr className="horizontal-line" />
+          </div>
+          <div className="copyright">
+            <div className="row">
+              <div className="col-12 d-flex justify-content-center">
+                <p className="footer-copyright">
+                  Plantilla Copyright © Portafolio{" "}
+                  <a
+                    className="footer-a"
+                    href="http://linkedin.com/in/mendietasergio"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Mendieta Sergio
+                  </a>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
